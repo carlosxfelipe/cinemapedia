4081729d38912e3d8314414a8497da148a0e4474
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2023-12-08 18:03:27 -0300

    movie screen wip

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,10 +1,17 @@
+import 'package:cinemapedia/presentation/screens/screens.dart';
 import 'package:go_router/go_router.dart';
-
-import 'package:cinemapedia/presentation/screens/movies/home_screen.dart';
 
 final appRouter = GoRouter(initialLocation: "/", routes: [
   GoRoute(
       path: "/",
       name: HomeScreen.name,
-      builder: (context, state) => const HomeScreen())
+      builder: (context, state) => const HomeScreen()),
+  GoRoute(
+      path: "/movie/:id",
+      name: MovieScreen.name,
+      builder: (context, state) {
+        final movieId = state.pathParameters['id'] ?? "no-id";
+
+        return MovieScreen(movieId: movieId);
+      })
 ]);
