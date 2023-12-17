3c5377240ef4ddacff15726ad68973f107ffd401
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2023-12-17 00:56:52 -0300

    get movie by id

--- lib/infrastructure/datasources/moviedb_datasource.dart
+++ lib/infrastructure/datasources/moviedb_datasource.dart
@@ -5,6 +5,7 @@ import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
 
 import 'package:cinemapedia/infrastructure/mappers/movie_mapper.dart';
 import 'package:cinemapedia/infrastructure/models/moviedb/moviedb_response.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_details.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 
 class MoviedbDatasource extends MoviesDatasource {
@@ -64,5 +65,20 @@ class MoviedbDatasource extends MoviesDatasource {
         await dio.get("/movie/top_rated", queryParameters: {"page": page});
 
     return _jsonToMovies(response.data);
+  }
+
+  @override
+  Future<Movie> getMovieById(String id) async {
+    final response = await dio.get("/movie/$id");
+
+    if (response.statusCode != 200) {
+      throw Exception("Movie with id: $id not found");
+    }
+
+    final movieDetails = MovieDetails.fromJson(response.data);
+
+    final Movie movie = MovieMapper.movieDetailsToEntity(movieDetails);
+
+    return movie;
   }
 }
