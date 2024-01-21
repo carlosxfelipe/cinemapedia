535c08bc6d12927e64016d5586ba0b65239d2e49
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2024-01-21 14:17:02 -0300

    search delegate

---
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SearchMovieDelegate extends SearchDelegate {
+  @override
+  String get searchFieldLabel => "Pesquisar filmes";
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [const Text("buildActions")];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return const Text("buildLeading");
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return const Text("buildResults");
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return const Text("buildSuggestions");
+  }
+}
--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cinemapedia/presentation/delegates/search_movie_delegate.dart';
 
 class CustomAppbar extends StatelessWidget {
   const CustomAppbar({super.key});
@@ -26,7 +27,12 @@ class CustomAppbar extends StatelessWidget {
                 style: titleStyle,
               ),
               const Spacer(),
-              IconButton(onPressed: () {}, icon: const Icon(Icons.search))
+              IconButton(
+                  onPressed: () {
+                    showSearch(
+                        context: context, delegate: SearchMovieDelegate());
+                  },
+                  icon: const Icon(Icons.search))
             ],
           ),
         ),
