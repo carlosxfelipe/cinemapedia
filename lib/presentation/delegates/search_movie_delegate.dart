8c5d17a0ec6f3ccff732e0ce547059243ceb01f8
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2024-01-27 10:32:08 -0300

    search movies wip

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,17 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:animate_do/animate_do.dart';
 
-class SearchMovieDelegate extends SearchDelegate {
+typedef SearchMoviesCallback = Future<List<Movie>> Function(String query);
+
+class SearchMovieDelegate extends SearchDelegate<Movie?> {
+  final SearchMoviesCallback searchMovies;
+
+  SearchMovieDelegate({required this.searchMovies});
+
   @override
   String get searchFieldLabel => "Pesquisar filmes";
 
   @override
   List<Widget>? buildActions(BuildContext context) {
-    return [const Text("buildActions")];
+    // return [const Text("buildActions")];
+    // print("query: $query");
+    return [
+      // if (query.isNotEmpty)
+      FadeIn(
+          animate: query.isNotEmpty,
+          duration: const Duration(milliseconds: 200),
+          child: IconButton(
+              onPressed: () => query = "", icon: const Icon(Icons.clear)))
+    ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
-    return const Text("buildLeading");
+    // return const Text("buildLeading");
+    return IconButton(
+        onPressed: () => close(context, null),
+        icon: const Icon(Icons.arrow_back_ios_new_rounded));
   }
 
   @override
@@ -21,6 +41,22 @@ class SearchMovieDelegate extends SearchDelegate {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return const Text("buildSuggestions");
+    // return const Text("buildSuggestions");
+    return FutureBuilder(
+      future: searchMovies(query),
+      builder: (context, snapshot) {
+        final movies = snapshot.data ?? [];
+
+        return ListView.builder(
+          itemCount: movies.length,
+          itemBuilder: (context, index) {
+            final movie = movies[index];
+            return ListTile(
+              title: Text(movie.title),
+            );
+          },
+        );
+      },
+    );
   }
 }
