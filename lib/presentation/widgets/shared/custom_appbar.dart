65ebd3b6e15969eb57d69b83e923786483c96a45
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2024-03-11 16:53:37 -0300

    search movies wip

--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/presentation/delegates/search_movie_delegate.dart';
 import 'package:cinemapedia/presentation/providers/movies/movies_repository_provider.dart';
 
@@ -32,10 +34,15 @@ class CustomAppbar extends ConsumerWidget {
               IconButton(
                   onPressed: () {
                     final movieRepository = ref.read(movieRepositoryProvider);
-                    showSearch(
-                        context: context,
-                        delegate: SearchMovieDelegate(
-                            searchMovies: movieRepository.searchMovies));
+                    showSearch<Movie?>(
+                            context: context,
+                            delegate: SearchMovieDelegate(
+                                searchMovies: movieRepository.searchMovies))
+                        .then((movie) {
+                      if (movie != null) {
+                        context.push("/movie/${movie.id}");
+                      }
+                    });
                   },
                   icon: const Icon(Icons.search))
             ],
